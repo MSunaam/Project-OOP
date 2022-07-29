889d6800d9be1e97b16e213490e0ac027e0ddf72
MSunaam <95274859+MSunaam@users.noreply.github.com>
2022-07-29 10:51:57 +0500

    update

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/muhammadsunaam/Desktop/PROJECT/untitled folder 2
+# Install script for directory: /Users/muhammadsunaam/Desktop/Project-OOP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/muhammadsunaam/Desktop/PROJECT/untitled folder 2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/muhammadsunaam/Desktop/Project-OOP/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
